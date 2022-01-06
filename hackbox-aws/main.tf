d94239f34bf244175a375129b3275077eeb44226
Shane Caldlwell <shanejcaldwell@gmail.com>
2022-01-05 20:10:22 -0600

    Kali AMI

---
+++ hackbox-aws/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "hack_server" {
+  ami           = "ami-0d12596b1b9089744"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HackboxServerInstance"
+  }
+}
+
